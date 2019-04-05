3e4429c353d6589032f1681847627722e7a9ccee
flytoro <49325365+flytoro@users.noreply.github.com>
2019-04-05 18:01:57 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ChristopherZepeda.py
@@ -0,0 +1,12 @@
+# Sales Prediction
+# 4-5-2019
+# CTI-110 P2T1 - Sales Prediction
+# Christopher Zepeda
+
+
+
+total_sales = float(input('Enter the projected sales: '))
+
+profit = total_sales * 0.23
+
+print('The profit is $', format(profit, ',.2f'))
